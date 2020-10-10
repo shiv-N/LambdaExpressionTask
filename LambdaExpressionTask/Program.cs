0a6d8f55f655d2b39f61bdbd6e42b2aee93a12b2
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2020-10-10 23:04:58 +0530

    Added : Create list and inserted as person record.(UC1)

--- LambdaExpressionTask/Program.cs
+++ LambdaExpressionTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LambdaExpressionTask
 {
@@ -6,7 +7,22 @@ namespace LambdaExpressionTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to record system.");
+            // UC 1: Create list and insert records.
+            List<Person> listPersonsInCity = new List<Person>();
+            AddRecords(listPersonsInCity);
+        }
+
+        private static void AddRecords(List<Person> listPersonsInCity)
+        {
+            listPersonsInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork, NY", 15));
+            listPersonsInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork, NY", 25));
+            listPersonsInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork, NY", 35));
+            listPersonsInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork, NY", 45));
+            listPersonsInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton, OH", 55));
+            listPersonsInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork, NY", 65));
+            listPersonsInCity.Add(new Person("203456882", "Winston", "1208 Alex St, Newyork, NY", 65));
+            listPersonsInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore, NY", 85));
+            listPersonsInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore, NY", 95));
         }
     }
 }
