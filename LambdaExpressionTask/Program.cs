c0645561f10cb758fb92ea3fd4d9962b92775182
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2020-10-10 22:51:36 +0530

    Added : A welcome message for record system.

---
+++ LambdaExpressionTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExpressionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to record system.");
+        }
+    }
+}
