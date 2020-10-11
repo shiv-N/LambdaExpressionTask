975c56b2bfa481ec4e503d3e0824bdc0db048902
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2020-10-11 20:43:11 +0530

    Refactored.

--- LambdaExpressionTask/Program.cs
+++ LambdaExpressionTask/Program.cs
@@ -12,18 +12,29 @@ namespace LambdaExpressionTask
             List<Person> listPersonsInCity = new List<Person>();
             AddRecords(listPersonsInCity);
 
-            Console.WriteLine("##Age");
-            AgeOperation age = new AgeOperation(listPersonsInCity);
-            age.AllAgeOperations();
+            // UC 2: Retrieving Top 2 aged persons from the list who are older than 60 years.
+            Console.WriteLine("\n-----------------------------------------------------------------------------");
+            Console.WriteLine("Retrieving Top 2 aged persons from the list who are older than 60 years\n");
+            Retrieving_TopTwoRecord_ForAgeIs_LessThanSixty(listPersonsInCity);
 
-            // UC 6: skips each person whose age is less than 60.
-            Console.WriteLine("\nSkipping every person whose age is less than 60 years...");
-            SkipRecordLessThanSixtyAge(listPersonsInCity);
+            // UC 3: Checks any person's age that is between 13 and 19 years.
+            Console.WriteLine("\n-----------------------------------------------------------------------------");
+            Console.WriteLine("\nChecking whether any person is teen-ager or not...");
+            CheckingForTeenagerPerson(listPersonsInCity);
+
+            // UC 4: Check average of all the person's age
+            Console.WriteLine("\n-----------------------------------------------------------------------------");
+            Console.WriteLine("\nGetting Average of all the person's age...");
+            AllPersonsAverageAge(listPersonsInCity);
 
             Console.WriteLine("\n## Name");
             // UC 5:Checking whether a person having name 'SAM' exists or not.
             Console.WriteLine("\nChecking whether a person having name 'SAM' exists or not...");
             CheckNameExistOrNot(listPersonsInCity);
+
+            // UC 6: skips each person whose age is less than 60.
+            Console.WriteLine("\nSkipping every person whose age is less than 60 years...");
+            SkipRecordLessThanSixtyAge(listPersonsInCity);
 
             // UC 7 :Removing all the persons record from list that have "SAM" name.
             Console.WriteLine("\nRemoving all the persons record from list that have 'SAM' name");
@@ -34,42 +45,6 @@ namespace LambdaExpressionTask
             Console.WriteLine("\nFinding the person whose SSN = 203456876 in the list");
             FindRecordBySSN(listPersonsInCity);
 
-        }
-
-        // UC 8
-        private static void FindRecordBySSN(List<Person> listPersonsInCity)
-        {
-            Person oPerson = listPersonsInCity.Find(e => (e.SSN == "203456876"));
-            Console.WriteLine("The person having SSN '203456876' is : " + oPerson.Name + " \t\tAge: " + oPerson.Age);
-        }
-
-        // UC 7
-        private static void RemoveRecord(List<Person> listPersonsInCity)
-        {
-            listPersonsInCity.RemoveAll(e => (e.Name == "SAM"));
-            if (listPersonsInCity.TrueForAll(e => e.Name != "SAM"))
-            {
-                Console.WriteLine("No person is found with 'SAM' name in current list");
-            }
-        }
-
-
-        // UC 6
-        private static void SkipRecordLessThanSixtyAge(List<Person> listPersonsInCity)
-        {
-            foreach (Person pers in listPersonsInCity.SkipWhile(e => e.Age < 60))
-            {
-                Console.WriteLine("Name : " + pers.Name + " \t\tAge: " + pers.Age);
-            }
-        }
-
-        // UC 5
-        private static void CheckNameExistOrNot(List<Person> listPersonsInCity)
-        {
-            if (listPersonsInCity.Exists(e => e.Name == "SAM"))
-            {
-                Console.WriteLine("Yes, A person having name  'SAM' exists in our list");
-            }
         }
 
         // UC 1
@@ -85,5 +60,68 @@ namespace LambdaExpressionTask
             listPersonsInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore, NY", 85));
             listPersonsInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore, NY", 95));
         }
+
+        // UC 2
+        private static void Retrieving_TopTwoRecord_ForAgeIs_LessThanSixty(List<Person> listPersonsInCity)
+        {
+            foreach (Person person in listPersonsInCity.FindAll(e => (e.Age >= 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name : " + person.Name + " \t\tAge: " + person.Age);
+            }
+        }
+
+        
+        // UC 3
+        private static void CheckingForTeenagerPerson(List<Person> listPersonsInCity)
+        {
+            if (listPersonsInCity.Any(e => (e.Age >= 13 && e.Age <= 19)))
+            {
+                Console.WriteLine("Yes, we have some teen-agers in the list");
+            }
+        }
+
+        // UC 4
+        private static void AllPersonsAverageAge(List<Person> listPersonsInCity)
+        {
+            double avgAge = listPersonsInCity.Average(e => e.Age);
+            Console.WriteLine("The average of all the person's age is: " + avgAge);
+        }
+
+        // UC 5
+        private static void CheckNameExistOrNot(List<Person> listPersonsInCity)
+        {
+            if (listPersonsInCity.Exists(e => e.Name == "SAM"))
+            {
+                Console.WriteLine("Yes, A person having name  'SAM' exists in our list");
+            }
+        }
+
+        // UC 6
+        private static void SkipRecordLessThanSixtyAge(List<Person> listPersonsInCity)
+        {
+            foreach (Person pers in listPersonsInCity.SkipWhile(e => e.Age < 60))
+            {
+                Console.WriteLine("Name : " + pers.Name + " \t\tAge: " + pers.Age);
+            }
+        }
+
+        
+        // UC 7
+        private static void RemoveRecord(List<Person> listPersonsInCity)
+        {
+            listPersonsInCity.RemoveAll(e => (e.Name == "SAM"));
+            if (listPersonsInCity.TrueForAll(e => e.Name != "SAM"))
+            {
+                Console.WriteLine("No person is found with 'SAM' name in current list");
+            }
+        }
+
+        // UC 8
+        private static void FindRecordBySSN(List<Person> listPersonsInCity)
+        {
+            Person oPerson = listPersonsInCity.Find(e => (e.SSN == "203456876"));
+            Console.WriteLine("The person having SSN '203456876' is : " + oPerson.Name + " \t\tAge: " + oPerson.Age);
+        }
+
     }
 }
